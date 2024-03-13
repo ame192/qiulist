f840c92ccb77853f280863852bdfc6021c301558
selu <selu@CERN.CH>
2024-03-13 22:47:44 +0100

    First Version

---
+++ script.js
@@ -0,0 +1,1160 @@
+var songs = [
+    
+  {
+    "name": "该",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "谁",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "问",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "空",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "落空",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "探窗",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "情歌",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "省略",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "针灸",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "人质",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "空空",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "中毒",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "冠玉",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "游京",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "演员",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "借过",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "山眠",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "来过",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "体面",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "白鸽",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "倒带",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "谪仙",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "失忆",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "草木",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "可能",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "雨爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "怀乡",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "奔赴",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "走马",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "冬眠",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "像鱼",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "暗恋",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "年轮",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "拜拜",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "唯一",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "虚拟",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "她说",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "宝贝",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "太阳",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "东西",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "偏爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "古怪",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "理查",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "遇见",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "后来",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "火苗",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "勇气",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "过火",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "艳火",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "原谅",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "春泥",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "稻香",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "雾里",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "雪藏",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "夜车",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "下潜",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "琴师",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "空心",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "慢慢",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "夏天",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "大眠",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "难却",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "再见",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "温泉",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "传奇",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "过活",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "渐冷",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "慢冷",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "红豆",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一个",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "黑桃A",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "多情种",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "别错过",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "全是爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "听说你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一袖云",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "讲真的",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "迷迭香",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "女儿情",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "若把你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "日不落",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我太笨",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "有点甜",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "明明就",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "忘尘谷",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "胡广生",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我要你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "虫儿飞",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "怎么了",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "静悄悄",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "芙蓉雨",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "从前说",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "云与海",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "小情歌",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "风筝误",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "外婆说",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "典狱司",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "七月上",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "想某人",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "大天蓬",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "桃花运",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "追光者",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "下雨天",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "四万秋",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "虞兮叹",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "春不晚",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "到不了",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "难生恨",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "恋人心",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "简单爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "伤不起",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "春风吹",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "红玫瑰",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "在深秋",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "声声慢",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "达尔文",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "牵丝戏",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "阿婆说",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "他不懂",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "带我走",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "水星记",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "叹郁孤",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我走后",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "王招君",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "外婆桥",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "球球泪",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "苏公堤",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "小幸运",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "丁香花",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "青花瓷",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "百利甜",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "向云端",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "Melody",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "爱丫爱丫",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "最后一页",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "金玉良缘",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "南山之上",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "荷塘月色",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "lovelove",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "New Boy",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我很快乐",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "爱似水仙",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "那么骄傲",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "曾经你说",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "忽然之间",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "她的睫毛",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "自娱自乐",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "身骑白马",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "死皮赖脸",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "不再联系",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "说散就散",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "摩登爱情",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "公主殿下",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "梦醒时分",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "到此为止",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "百万个吻",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "天会亮吗",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "阴天快乐",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "往后余生",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "不负人间",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "岁月神偷",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一路生花",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "失落的缘",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "漠河舞厅",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "理想三旬",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "见与不见",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "天空之外",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "无人之岛",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "千年之恋",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一笑江湖",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "光年之外",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "说好不哭",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "爱的魔法",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "见与不见",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "不问别离",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我好想你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "修炼爱情",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "三拜红尘凉",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "可惜没如果",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "苍狗与海鸥",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "勇气大爆发",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "奇妙能力歌",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "越来越不懂",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "可惜不是你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "最炫民族风",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我配不上你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "背对背拥抱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "会呼吸的痛",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "慢慢喜欢你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "想念拟人化",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "想你这件事",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我的歌声里",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一直很安静",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "如果爱忘了",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "死了都要爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "盛夏的果实",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "幸福了然后呢",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "无人知晓的我",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "什么样的爱情",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "火红的萨日朗",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "阿拉斯加海湾",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "永不失联的爱",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "去有风的地方",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "从开始到现在",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "银河爱情故事",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "等到世界颠倒",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "满天星辰不及你",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "你的眼睛像星星",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "这世界那么多人",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "你就不要想起我",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "白月光与朱砂痣",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "当想你成为习惯",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "带我去很远地方",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "我想对你说Baby",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "一个人想着一个人",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "路边的野花不要采",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "在加纳共和国离婚",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "就让这大雨全都落下",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "山茶花读不懂白玫瑰",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "有个爱你的人不容易",
+    "artist": "群星",
+    "genre": "流行"
+  },
+  {
+    "name": "写给我第一个喜欢的女孩的歌",
+    "artist": "群星",
+    "genre": "流行"
+  }
+    // Add more song objects following this format
+];
+
+var searchInput = document.getElementById('search-input');
+var genreSelect = document.getElementById('genre-select');
+var songList = document.getElementById('song-list');
+var songDetails = document.getElementById('song-details');
+
+// Function to populate genre dropdown
+function populategenreSelect() {
+    var genres = Array.from(new Set(songs.map(song => song.genre)));
+
+    var options = genres.map(function(genre) {
+        return '<option value="'+genre+'">'+genre+'</option>';
+    });
+
+    genreSelect.innerHTML = '<option value="">类别</option>' + options.join('');
+}
+
+// Function to display the songs on the HTML page
+function renderSongs(filteredSongs) {
+    var html = '';
+    filteredSongs.forEach(function (song) {
+        html += '<tr><td>' + song.name + '</td><td>' + song.artist + '</td><td>' + song.genre + '</td></tr>';
+    });
+    songDetails.innerHTML = html;
+}
+
+// Function to filter songs based on the search input and the selected genre
+function filterSongs() {
+    var searchInputValue = searchInput.value.toLowerCase();
+    var genreSelectValue = genreSelect.value;
+    var filteredSongs = songs.filter(function(song) {
+        var hasMatchingname = searchInputValue ?
+            // If the user has typed something in the search field then filter by song name or artist name, else render all songs
+            song.name.toLowerCase().includes(searchInputValue) || song.artist.toLowerCase().includes(searchInputValue) :
+            true;
+        var hasMatchinggenre = genreSelectValue ?
+            // If the user has selected a genre then filter by genre, else render all songs
+            song.genre === genreSelectValue :
+            true;
+        return hasMatchingname && hasMatchinggenre;
+    });
+    renderSongs(filteredSongs);
+}
+
+// React to changes in the search field and the dropdown select field
+searchInput.addEventListener('input', filterSongs);
+genreSelect.addEventListener('change', filterSongs);
+
+// Initial population of the genre dropdown and rendering of the song lists
+populategenreSelect();
+filterSongs();
