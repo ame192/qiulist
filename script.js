458aab56ac9705520b6393916a112fa0c53ad071
selu <selu@CERN.CH>
2024-03-19 22:06:28 +0100

    seems ok

--- script.js
+++ script.js
@@ -20,47 +20,57 @@ var app = new Vue({
             });
         }
     },
-    methods: {
-        copyToClipboard(songName) {
-            navigator.clipboard.writeText(songName)
-                .then(() => {
-                    console.log('Song name copied to the clipboard:', songName);
-                })
-                .catch((error) => {
-                    console.error('Error copying song name to clipboard:', error);
-                });
-        },
-        parseCSV(file) {
-            Papa.parse(file, {
-                header: true,
-                complete: (results) => {
-                    this.songs = results.data;
-                    this.genres = this.getUniqueGenres(this.songs);
-                }
-            });
-        },
-        getUniqueGenres(songs) {
-            var genres = [];
-            songs.forEach(function(song) {
-                if (!genres.includes(song.genre)) {
-                    genres.push(song.genre);
-                }
-            });
-            return genres;
-        }
-    },
-    mounted() {
-        // CSV file path
-        var csvFilePath = 'songlist.csv';
+  methods: {
+    copyToClipboard(songName) {
+      var tempInput = document.createElement("input");
+      tempInput.setAttribute("value", songName);
+      document.body.appendChild(tempInput);
+      tempInput.select();
 
-        // Make a fetch request to get the CSV file
-        fetch(csvFilePath)
-            .then(response => response.text())
-            .then(data => {
-                this.parseCSV(data);
-            })
-            .catch(error => {
-                console.error('Error fetching CSV file:', error);
-            });
-    }
+      var clipboard = new ClipboardJS(tempInput);
+      clipboard.on("success", () => {
+        console.log('Song name copied to the clipboard:', songName);
+        clipboard.destroy();
+        document.body.removeChild(tempInput);
+      });
+
+      tempInput.focus();
+      document.execCommand("copy");
+    },
+    parseCSV(file) {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          this.songs = results.data;
+          this.genres = this.getUniqueGenres(this.songs);
+        }
+      });
+    },
+    getUniqueGenres(songs) {
+      var genres = [];
+      songs.forEach(function (song) {
+        if (!genres.includes(song.genre)) {
+          genres.push(song.genre);
+        }
+      });
+      return genres;
+    },
+    navigateTo(path) {
+            window.location.href = path;
+    },
+  },
+  mounted() {
+    // CSV file path
+    var csvFilePath = 'songlist.csv';
+
+    // Make a fetch request to get the CSV file
+    fetch(csvFilePath)
+      .then(response => response.text())
+      .then(data => {
+        this.parseCSV(data);
+      })
+      .catch(error => {
+        console.error('Error fetching CSV file:', error);
+      });
+  }
 });
